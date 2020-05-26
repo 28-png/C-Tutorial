5762e8835332f7eab94be00c597629b1e9670685
Matt Murphy <murphy.d.matthew28@gmail.com>
2020-05-26 14:56:28 -0500

    first commit

---
+++ cmake-build-debug/CMakeFiles/c__tutorial.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c__tutorial.dir/main.c.o"
+  "c__tutorial"
+  "c__tutorial.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c__tutorial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
